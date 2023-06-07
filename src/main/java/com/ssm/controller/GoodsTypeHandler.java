3b2c2b60af5b063df4b9615f4bb6f2c128cfaa03
hhrwvyy5654v <huang_rongquan@outlook.com>
2023-06-07 11:17:37 +0800

    annotate CONTROLLER

--- src/main/java/com/ssm/controller/GoodsTypeHandler.java
+++ src/main/java/com/ssm/controller/GoodsTypeHandler.java
@@ -1,3 +1,12 @@
+/*
+ * @Description: 基于Spring MVC框架的商品控制器类，用于处理与商品类型相关的请求
+ * @FilePath: \src\main\java\com\ssm\controller\GoodsTypeHandler.java
+ * @Author: hhrwvyy5654v huang_rongquan@outlook.com
+ * @Date: 2023-06-05 11:32:03
+ * @LastEditors: hhrwvyy5654v huang_rongquan@outlook.com
+ * @LastEditTime: 2023-06-07 10:08:36
+ * Copyright (c) 2023 by hhrwvyy5654v , All Rights Reserved. 
+ */
 package com.ssm.controller;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,57 +19,69 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.ssm.po.GoodsType;
 import com.ssm.service.inter.TypeServiceInter;
 
-@Controller
+@Controller // 标识该类是一个控制器
 public class GoodsTypeHandler {
-	@Autowired
+	@Autowired // 将TypeServiceInter类型的对象注入到该类中
 	private TypeServiceInter typeInetr;
-	
-	@RequestMapping(value="/getGoods.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String goodsIndex() throws Exception{
+
+	// @RequestMapping注解的value属性指请求的路径，produces属性指定响应的MIME类型和字符集编码。
+	@RequestMapping(value = "/getGoods.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解将返回的数据直接写入响应体中
+	// 用于处理"/getGoods.do"请求，返回商品类型的JSON数据
+	public String goodsIndex() throws Exception {
 		String goods = typeInetr.goodsIndex();
 		return goods;
 	}
-	
-	
-	@RequestMapping(value="/del_types.do")
-	@ResponseBody
-	public String del_types(Integer id,Integer shiro_id) throws Exception{
 
+	// @RequestMapping注解的value属性指请求的路径
+	@RequestMapping(value = "/del_types.do")
+	@ResponseBody // 使用@ResponseBody注解将返回的数据直接写入响应体中
+	// 用于将指定id的商品类型从数据库中删除
+	public String del_types(Integer id, Integer shiro_id) throws Exception {
 		return typeInetr.admin_delType(id, shiro_id);
 	}
-	
-	
-	@RequestMapping(value="/addType.do")
-	public void addType(GoodsType type,HttpServletResponse response) throws Exception{
-		boolean flag = typeInetr.AdminAddType(type);
-		if(flag) {
-			response.sendRedirect("/Shop-SSM/admin/goods_sort.html");
-		}else {
-			response.setContentType("text/html;charset=utf-8");
-			response.getWriter().println("<script>alert('添加失败！');location.href='/Shop-SSM/admin/add_type.html';</script>");
-		}
-		
-	}
-	
-	@RequestMapping(value="/AdminGetType.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String AdminGetType(Integer id) throws Exception{
 
+	// 使用@RequestMapping注解将请求映射到"/addType.do"路径上
+	@RequestMapping(value = "/addType.do")
+	// 用于将新的商品类型添加到数据库中，并根据添加结果重定向或输出响应内容
+	public void addType(GoodsType type, HttpServletResponse response) throws Exception {
+		// 调用typeInetr.AdminAddType(type)方法，将商品类型添加到数据库中
+		boolean flag = typeInetr.AdminAddType(type);
+		if (flag) {
+			// 如果添加成功，则使用response.sendRedirect()重定向到"/Shop-SSM/admin/goods_sort.html"页面
+			response.sendRedirect("/Shop-SSM/admin/goods_sort.html");
+		} else {
+			// 否则使用response.setContentType()方法设置响应的MIME类型和字符集编码
+			response.setContentType("text/html;charset=utf-8");
+			// 然后使用response.getWriter()方法获取输出流，输出一个JavaScript弹窗提示添加失败，并使用location.href属性将页面重定向到"/Shop-SSM/admin/add_type.html"页面
+			response.getWriter()
+					.println("<script>alert('添加失败！');location.href='/Shop-SSM/admin/add_type.html';</script>");
+		}
+	}
+
+	// @RequestMapping注解的value属性指请求的路径，produces指定响应的MIME类型和字符集编码
+	@RequestMapping(value = "/AdminGetType.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解将返回的数据直接写入响应体中
+	// 用于获取指定id的商品类型,并将返回的字符串会被直接发送给客户端
+	public String AdminGetType(Integer id) throws Exception {
 		return typeInetr.AdminGetIdType(id);
 	}
-	
-	
-	@RequestMapping(value="/AdminUpdateType.do")
-	public void AdminUpdateType(GoodsType type,HttpServletResponse response) throws Exception{
-		boolean flag = typeInetr.AdminUpdateTypes(type);
-		if(flag) {
-			response.sendRedirect("/Shop-SSM/admin/goods_sort.html");
-		}else {
-			response.setContentType("text/html;charset=utf-8");
-			response.getWriter().println("<script>alert('修改失败！');location.href='/Shop-SSM/admin/goods_sort.html';</script>");
-		}
-		
-	}
 
+	// 使用@RequestMapping注解将请求映射到"/AdminUpdateType.do"路径上
+	@RequestMapping(value = "/AdminUpdateType.do")
+	// 用于将修改后的商品类型更新到数据库中，并根据更新结果重定向或输出响应内容
+	public void AdminUpdateType(GoodsType type, HttpServletResponse response) throws Exception {
+		// 调用typeInetr.AdminUpdateTypes(type)方法，将商品类型更新到数据库中
+		boolean flag = typeInetr.AdminUpdateTypes(type);
+		if (flag) {
+			// 如果更新成功，则使用response()重定向到"/Shop-SSM/admin/goods_sort.html"页面
+			response.sendRedirect("/Shop-SSM/admin/goods_sort.html");
+		} else {
+			// 否则使用response.setContentType()方法设置响应的MIME类型和字符集编码
+			response.setContentType("text/html;charset=utf-8");
+			// 然后使用response.getWriter()方法获取输出流，输出一个JavaScript弹窗提示修改失败，并使用location.href属性将页面重定向到"/Shop-SSM/admin/goods_sort.html"页面
+			response.getWriter()
+					.println("<script>alert('修改失败！');location.href='/Shop-SSM/admin/goods_sort.html';</script>");
+		}
+	}
 }
--- src/main/java/com/ssm/controller/OrdersHandler.java
+++ src/main/java/com/ssm/controller/OrdersHandler.java
@@ -1,3 +1,12 @@
+/*
+ * @Description: 基于Spring MVC框架的订单控制器类，用于处理与订单相关的请求
+ * @FilePath: \src\main\java\com\ssm\controller\OrdersHandler.java
+ * @Author: hhrwvyy5654v huang_rongquan@outlook.com
+ * @Date: 2023-06-05 11:32:03
+ * @LastEditors: hhrwvyy5654v huang_rongquan@outlook.com
+ * @LastEditTime: 2023-06-07 10:35:58
+ * Copyright (c) 2023 by hhrwvyy5654v , All Rights Reserved. 
+ */
 package com.ssm.controller;
 
 import java.net.URLDecoder;
@@ -10,96 +19,111 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssm.service.inter.OrdersServiceInter;
 
-@Controller
+@Controller // 标识该类是一个控制器
 public class OrdersHandler {
+	// 使用@Autowired注解自动注入OrdersServiceInter的实例
 	@Autowired
 	private OrdersServiceInter ordersServiceInter;
-	
+
+	// 使用@RequestMapping注解将请求映射到"/getCounts.do"路径上
 	@RequestMapping("/getCounts.do")
-	@ResponseBody
-	public String getCounts(Integer num) throws Exception{
-		
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于获取订单数量，并将结果以字符串形式返回给客户端
+	public String getCounts(Integer num) throws Exception {
 		return ordersServiceInter.getCounts(num);
 	}
-	
-	@RequestMapping(value="/FenyeLists.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String FenyeLists(Integer page,Integer shiro_id) throws Exception{
-		
-		return ordersServiceInter.FenyeList(page,shiro_id);
+
+	// 使用@RequestMapping注解将请求映射到"/FenyeLists.do"路径上,使用"produces"属性设置响应的MIME类型"text/html;charset=UTF-8;"，以确保响应文的编码正确
+	@RequestMapping(value = "/FenyeLists.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于分页获取订单列表，并将结果以字符串形式返回给客户端
+	public String FenyeLists(Integer page, Integer shiro_id) throws Exception {
+		return ordersServiceInter.FenyeList(page, shiro_id);
 	}
-	
-	
+
+	// 使用@RequestMapping注解将请求映射到"/delByOrderId.do"路径上
 	@RequestMapping("/delByOrderId.do")
-	@ResponseBody
-	public String delByOrderId(Integer id) throws Exception{
-		
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于删除订单，并将结果以字符串形式返回给客户端
+	public String delByOrderId(Integer id) throws Exception {
 		return ordersServiceInter.delByidOrders(id);
 	}
-	
-	
+
+	// 使用@RequestMapping注解将请求映射到"/pay_order.do"路径上
 	@RequestMapping("/pay_order.do")
-	@ResponseBody
-	public String pay_order(Integer id) throws Exception{
-		
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于支付订单，并将结果以字符串形式返回给客户端
+	public String pay_order(Integer id) throws Exception {
 		return ordersServiceInter.PayOrders(id);
 	}
-	
-	
+
+	// 使用@RequestMapping注解将请求映射到"/paysDing.do"路径上
 	@RequestMapping("/paysDing.do")
-	@ResponseBody
-	public String paysDing(@RequestBody String lists) throws Exception{
-		lists=URLDecoder.decode(lists, "utf-8");//转化，不然会乱码
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于添加订单，并将结果以字符串形式返回给客户端
+	public String paysDing(@RequestBody String lists) throws Exception {
+		lists = URLDecoder.decode(lists, "utf-8");// 解码以避免出现乱码
 		return ordersServiceInter.addOrders(lists);
 	}
-	
-	
-	@RequestMapping(value="/adminDaiPays.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
+
+	// 使用@RequestMapping注解将HTTP请求映射到"/adminDaiPays.do"路径上,使用"produces"属性设置响应的MIME类型"text/html;charset=UTF-8;"，以确保响应文的编码正确
+	@RequestMapping(value = "/adminDaiPays.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于查询待支付订单，并将结果以字符串形式返回给客户端
 	public String adminDaiPays(Integer status) throws Exception {
 		return ordersServiceInter.adminDaipays(status);
 	}
-	
-	
-	
-	@RequestMapping(value="/adminDaiDels.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String adminDaiDels(Integer id,Integer shiro_id) throws Exception {
-		if(id==null||shiro_id==null) {
+
+	// 使用@RequestMapping注解将HTTP请求映射到"/adminDaiDels.do"路径上,使用"produces"属性设置响应的MIME类型"text/html;charset=UTF-8;"，以确保响应文的编码正确
+	@RequestMapping(value = "/adminDaiDels.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于删除订单,并将结果以字符串形式返回给客户端
+	public String adminDaiDels(Integer id, Integer shiro_id) throws Exception {
+		// 首先检查"id"和"shiro_id"是否为null
+		if (id == null || shiro_id == null) {
+			// 如果其中任何一个为null，则返回一个JavaScript脚本，该脚本在客户端弹出一个警告框，然后将页面重定向到"/Shop-SSM/admin/orders_dai.html"路径
 			return "<script>alert('非法请求');location.href='/Shop-SSM/admin/orders_dai.html';</script>";
-		}else {
+		} else {
+			// 如果"id"和"shiro_id"都不为null，则将它们传递给ordersServiceInter.adminDelsOrders()方法进行处理
 			return ordersServiceInter.adminDelsOrders(id, shiro_id);
 		}
-		
 	}
-	
-	@RequestMapping(value="/adminlogisticsOrdes.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String adminlogisticsOrdes(String status) throws Exception{
+
+	// 使用@RequestMapping注解将HTTP请求映射到"/adminlogisticsOrdes.do"路径上,使用"produces"属性设置响应的MIME类型"text/html;charset=UTF-8;"，以确保响应文的编码正确
+	@RequestMapping(value = "/adminlogisticsOrdes.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于查询物流信息，并将结果以字符串形式返回给客户端
+	public String adminlogisticsOrdes(String status) throws Exception {
 		return ordersServiceInter.adminLogistics(status);
-		
 	}
-	
-	@RequestMapping(value="/adminfahuoOrdes.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String adminfahuoOrdes(Integer id) throws Exception{
-		if(id==null) {
+
+	// 使用@RequestMapping注解将HTTP请求映射到"/adminfahuoOrdes.do"路径上,使用"produces"属性设置响应的MIME类型"text/html;charset=UTF-8;"，以确保响应文的编码正确
+	@RequestMapping(value = "/adminfahuoOrdes.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于发货，并将结果以字符串形式返回给客户端
+	public String adminfahuoOrdes(Integer id) throws Exception {
+		// 检查"id"是否为null
+		if (id == null) {
+			// 如果"id"为null，则返回一个JavaScript脚本，该脚本在客户端弹出一个警告框，然后将页面重定向到"/Shop-SSM/admin/orders_weifa.html"路径
 			return "<script>alert('非法请求');location.href='/Shop-SSM/admin/orders_weifa.html';</script>";
-		}else {
-			boolean falg = ordersServiceInter.adminFaOrdes(id);
-			if(falg) {
+		} else {
+			// 如果"id"不为null，则将其传递给ordersServiceInter.adminFaOrdes()方法进行处理
+			boolean flag = ordersServiceInter.adminFaOrdes(id);
+			if (flag) {
+				// 则返回一个JavaScript脚本，该脚本在客户端弹出一个提示框，显示"发货成功"，然后将页面重定向到"/Shop-SSM/admin/ordersifa.html"路径
 				return "<script>alert('发货成功');location.href='/Shop-SSM/admin/orders_weifa.html';</script>";
-			}else {
+			} else {
+				// 如果ordersServiceInter.adminFaOrdes()方法返回false，则返回一个JavaScript脚本，该脚本在客户端弹出一个提示框，显示"发货失败"，然后将页面重定向到"/Shop-SS/admin/orders_weifa.html"路径
 				return "<script>alert('发货失败');location.href='/Shop-SSM/admin/orders_weifa.html';</script>";
 			}
 		}
 	}
-	
-	
-	@RequestMapping(value="/AdminGtByorderId.do",produces="text/html;charset=UTF-8;")
-	@ResponseBody
-	public String AdminGtByorderId(Integer id) throws Exception{
+
+	// 使用@RequestMapping注解将HTTP请求映射到"/AdminGtByorderId.do"路径上,使用"produces"属性设置响应的MIME类型"text/html;charset=UTF-8;"，以确保响应文的编码正确
+	@RequestMapping(value = "/AdminGtByorderId.do", produces = "text/html;charset=UTF-8;")
+	@ResponseBody // 使用@ResponseBody注解表示返回的数据直接写入响应体中
+	// 用于查询订单信息，并将结果以字符串形式返回给客户端
+	public String AdminGtByorderId(Integer id) throws Exception {
 		return ordersServiceInter.adminGetords(id);
 	}
-
 }
